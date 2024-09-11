fc61a27d29786a1566bdda51ba4cc631420970bb
yashkerkar123 <spotify.k321@gmail.com>
2024-09-11 17:16:52 +0530

    Delete src directory

--- src/main/java/com/example/library/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/library/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.repository;
-
-import com.example.library.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
